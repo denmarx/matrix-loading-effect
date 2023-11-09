4feecd1f366437a09e8e152fd1ae6729380e468e
undeadgorilla <dennis.marx93@gmail.com>
2023-11-09 20:39:40 +0100

    animation and resize behaviour

--- main.js
+++ main.js
@@ -15,9 +15,8 @@ class Symbol {
   }
   draw(context) {
     this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
-    context.fillStyle = 'rgb(22, 89, 177)';
-    context.fillStyle(this.text, this.x * this.fontSize, this.y * this.fontSize);
-    if (this.y * this.fontSize > this.canvasHeight) {
+    context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+    if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
       this.y = 0;
     } else {
       this.y += 1;
@@ -32,16 +31,49 @@ class Effect {
     this.fontSize = 25;
     this.columns = this.canvasWidth / this.fontSize;
     this.symbols = [];
+    this.#initialize();
   }
   #initialize() {
-    for (let i = 0; i < this.columns.length; i++) {
+    for (let i = 0; i < this.columns; i++) {
       this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
     }
+  }
+  resize(width, height) {
+    this.canvasWidth = width;
+    this.canvasHeight = height;
+    this.columns = this.canvasWidth / this.fontSize;
+    this.symbols = [];
+    this.#initialize();
   }
 }
 
 const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 30;
+const nextFrame = 1000 / fps;
+let timer = 0;
 
-function animate() {
-  ctx.font = effect.fontSize + 'px monospace';
+function animate(timeStamp) {
+  const deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+  if (timer > nextFrame) {
+    // ctx.font specifies current text style
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    ctx.textAlign = 'center';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgb(22, 89, 177)';
+    ctx.font = effect.fontSize + 'px monospace';
+    effect.symbols.forEach((symbol) => symbol.draw(ctx));
+    timer = 0;
+  } else {
+    timer += deltaTime;
+  }
+  requestAnimationFrame(animate);
 }
+animate(0);
+
+window.addEventListener('resize', function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  effect.resize(canvas.width, canvas.height);
+});
